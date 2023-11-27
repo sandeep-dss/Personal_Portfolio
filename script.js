3e8f59508cc74065ff8d4559b8374f81971fd2b8
Sandeep Desaraju <71257134+sandeep-dss@users.noreply.github.com>
2023-11-27 20:35:39 +0530

    animations added

---
+++ script.js
@@ -0,0 +1,23 @@
+// hero section animations
+
+gsap.fromTo("#name", {x: -500}, {x: 1, duration: 1, ease : "power3.out"});
+
+gsap.fromTo("#jobstatus", {x: -500}, {x: 1, duration: 1, ease : "power3.out"});
+
+gsap.fromTo("#herotext", {x: -500}, {x: 1,  duration: 1, ease : "power3.inOut", delay : 0.3});
+
+// gsap.fromTo("#downloadbtn", {x: -800}, {x: 1, duration: 0.8});
+
+gsap.fromTo("#heroimg", {x: 1000}, {x: 1, duration: 1, ease : "power3.out", delay : 0.8});
+
+
+//navigation aimations
+
+gsap.fromTo("#navhead", {y: -500}, {y: 1, duration: 0.8});
+
+gsap.fromTo("#navimg", {y: -500}, {y: 1, duration: 0.8});
+
+gsap.fromTo("#navitem", {y: -500}, {y: 1, duration: 0.8});
+
+// scroll animations
+
