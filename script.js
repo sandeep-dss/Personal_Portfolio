2a0dd0594afb9c972333dd1e7bc757f34334400c
Sandeep Desaraju <71257134+sandeep-dss@users.noreply.github.com>
2023-11-29 21:23:01 +0530

    animations added

--- script.js
+++ script.js
@@ -21,3 +21,85 @@ gsap.fromTo("#navitem", {y: -500}, {y: 1, duration: 0.8});
 
 // scroll animations
 
+gsap.registerPlugin(ScrollTrigger);
+
+if(window.innerWidth > 768){
+gsap.from('#skills', {
+    opacity: 0,
+    x: -500,
+    duration: 1,
+    stagger: 0.2, // Adjust the stagger value based on your preference
+    scrollTrigger: {
+      trigger: '#skills', // Element that triggers the animation
+      start: 'top 100%', // Adjust the start position based on your preference
+    },
+  });
+
+  gsap.from('#workexp', {
+    opacity: 0,
+    x: 500,
+    duration: 1,
+    stagger: 0.2, // Adjust the stagger value based on your preference
+    scrollTrigger: {
+      trigger: '#workexp', // Element that triggers the animation
+      start: 'top 100%', // Adjust the start position based on your preference
+    },
+  });
+
+  gsap.from('#project', {
+    opacity: 0,
+    x: -500,
+    duration: 1,
+    stagger: 0.2, // Adjust the stagger value based on your preference
+    scrollTrigger: {
+      trigger: '#project', // Element that triggers the animation
+      start: 'top 100%', // Adjust the start position based on your preference
+    },
+  });
+
+  gsap.from('#hobby', {
+    opacity: 0,
+    y: 500,
+    duration: 1,
+    stagger: 0.2, // Adjust the stagger value based on your preference
+    scrollTrigger: {
+      trigger: '#hobby', // Element that triggers the animation
+      start: 'top 100%', // Adjust the start position based on your preference
+    },
+  });
+}
+
+else{
+    gsap.from('#skills', {
+        opacity: 0,
+        y: 500,
+        duration: 1,
+        stagger: 0.2, // Adjust the stagger value based on your preference
+        scrollTrigger: {
+          trigger: '#skills', // Element that triggers the animation
+          start: 'top 100%', // Adjust the start position based on your preference
+        },
+      });
+    
+      gsap.from('#workexp', {
+        opacity: 0,
+        y: 500,
+        duration: 1,
+        stagger: 0.2, // Adjust the stagger value based on your preference
+        scrollTrigger: {
+          trigger: '#workexp', // Element that triggers the animation
+          start: 'top 100%', // Adjust the start position based on your preference
+        },
+      });
+    
+      gsap.from('#project', {
+        opacity: 0,
+        y: 500,
+        duration: 1,
+        stagger: 0.2, // Adjust the stagger value based on your preference
+        scrollTrigger: {
+          trigger: '#project', // Element that triggers the animation
+          start: 'top 100%', // Adjust the start position based on your preference
+        },
+      });
+}
